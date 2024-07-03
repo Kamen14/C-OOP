146436f1623b76bbc36f6d08936d76293a6dceb3
Kamen14 <chirpanlievkamen@gmail.com>
2024-07-03 21:07:22 +0300

    Add files via upload

---
+++ Citizen.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Citizen(string name, int age, string id, string birthDate) : this(name, age)
+        {
+            Id = id;
+            Birthdate = birthDate;
+        }
+
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public string Id { get; }
+
+        public string Birthdate {  get; }
+    }
+}
